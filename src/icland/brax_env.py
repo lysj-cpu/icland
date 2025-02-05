574ab07a648670d97da696c63e700c1cebab11bf
harens <12570877+harens@users.noreply.github.com>
2025-02-05 17:35:47 +0000

    refactor: simplify brax/icland adapters

--- src/icland/brax_env.py
+++ src/icland/brax_env.py
@@ -3,85 +3,41 @@
 from typing import Optional
 
 import jax
-from brax.base import Motion, System, Transform
-from brax.envs.base import Env, ObservationSize, State
+from brax.envs.base import Env, ObservationSize
 from brax.io import mjcf
-from brax.mjx.base import State as mjx_state
-from brax.mjx.pipeline import _reformat_contact
 
 import icland
-
-
-def icland_to_brax_adapter(icland_state: icland.ICLandState) -> State:
-    """Converts an ICLandState to a Brax base State."""
-    model = icland_state.pipeline_state.mjx_model
-    data = icland_state.pipeline_state.mjx_data
-
-    # Adapted from m_pipeline.init (brax/mjx/pipeline.py)
-    # Its init function calls mjx.forward, which crashes
-    q, qd = data.qpos, data.qvel
-    x = Transform(pos=data.xpos[1:], rot=data.xquat[1:])
-    cvel = Motion(vel=data.cvel[1:, 3:], ang=data.cvel[1:, :3])
-    offset = data.xpos[1:, :] - data.subtree_com[model.body_rootid[1:]]
-    offset = Transform.create(pos=offset)
-    xd = offset.vmap().do(cvel)
-    reformated_data = _reformat_contact(model, data)
-
-    return State(
-        pipeline_state=mjx_state(q=q, qd=qd, x=x, xd=xd, **reformated_data.__dict__),
-        obs=icland_state.observation,
-        reward=icland_state.reward,
-        done=icland_state.done,
-        metrics=icland_state.metrics,
-    )
-
-
-def brax_to_icland_adapter(
-    brax_state: State, model: System, agent_count: int
-) -> icland.ICLandState:
-    """Converts a Brax base State to an ICLandState."""
-    # In m_pipeline.step, mjx.step is called directly on the brax state
-    return icland.ICLandState(
-        pipeline_state=icland.PipelineState(
-            mjx_model=model,
-            mjx_data=brax_state.pipeline_state,
-            # TODO: Consider caching agent calculation
-            component_ids=icland.collect_agent_components(model, agent_count),
-        ),
-        observation=brax_state.obs,
-        reward=brax_state.reward,
-        done=brax_state.done,
-        metrics=brax_state.metrics,
-        info=brax_state.info,
-    )
 
 
 class ICLand(Env):  # type: ignore[misc]
     """ICLand Brax environment."""
 
-    def __init__(self, rng: jax.Array, params: Optional[icland.ICLandParams]) -> None:
+    def __init__(
+        self, rng: jax.Array, params: Optional[icland.ICLandParams] = None
+    ) -> None:
         """Initializes the environment with a random seed."""
         if params is None:
             params = icland.sample(rng)
         self.params = params
         self._sys = mjcf.load_model(self.params.model)
 
-    def reset(self, rng: jax.Array) -> State:
+    def reset(self, rng: jax.Array) -> icland.ICLandBraxState:
         """Resets the environment to an initial state."""
         initial_state = icland.init(rng, self.params)
-        return icland_to_brax_adapter(initial_state)
+        return initial_state.to_brax_state()
 
-    def step(self, state: State, action: jax.Array) -> State:
+    def step(
+        self, state: icland.ICLandBraxState, action: jax.Array
+    ) -> icland.ICLandBraxState:
         """Run one timestep of the environment's dynamics."""
         key = jax.random.PRNGKey(0)
-        return icland_to_brax_adapter(
-            icland.step(
-                key,
-                brax_to_icland_adapter(state, self._sys, self.params.agent_count),
-                self.params,
-                action,
-            )
+        icland_state: icland.ICLandState = icland.step(
+            key,
+            state.to_icland_state(),
+            self.params,
+            action,
         )
+        return icland_state.to_brax_state()
 
     @property
     def observation_size(self) -> ObservationSize:
--- src/icland/types.py
+++ src/icland/types.py
@@ -4,11 +4,15 @@ It includes types for model parameters, state, and action sets used in the proje
 """
 
 import inspect
-from typing import Callable, Optional, TypeAlias
+from typing import Callable, Optional, Type, TypeAlias
 
 import jax
 import jax.numpy as jnp
 import mujoco
+from brax.base import Motion, System, Transform
+from brax.envs.base import State
+from brax.mjx.base import State as mjx_state
+from brax.mjx.pipeline import _reformat_contact
 from mujoco.mjx._src.dataclasses import PyTreeNode
 
 """Type variables from external modules."""
@@ -33,7 +37,17 @@ class PipelineState(PyTreeNode):  # type: ignore[misc]
     component_ids: jnp.ndarray
 
 
-class ICLandState(PyTreeNode):  # type: ignore[misc]
+type ICLandStateChildren = tuple[
+    PipelineState,
+    jax.Array,
+    jax.Array,
+    jax.Array,
+    dict[str, jax.Array],
+    dict[str, jax.Array],
+]
+
+
+class ICLandState:
     """Information regarding the current step.
 
     Attributes:
@@ -45,14 +59,70 @@ class ICLandState(PyTreeNode):  # type: ignore[misc]
         info: Dictionary of additional information.
     """
 
-    pipeline_state: PipelineState
-    observation: jax.Array
-    reward: jax.Array
-    done: jax.Array
-    # metrics: jax.Array  # These were Dict[str, jax.Array] = struct.field(default_factory=dict) but we need to install flax
-    # info: jax.Array  # These were Dict[str, Any] = struct.field(default_factory=dict) but we need to install flax
-    metrics: dict[str, jax.Array]
-    info: dict[str, jax.Array]
+    def __init__(
+        self,
+        pipeline_state: PipelineState,
+        observation: jax.Array,
+        reward: jax.Array,
+        done: jax.Array,
+        metrics: dict[str, jax.Array],
+        info: dict[str, jax.Array],
+    ) -> None:
+        """Initializes the state with the given values."""
+        self.pipeline_state = pipeline_state
+        self.observation = observation
+        self.reward = reward
+        self.done = done
+        self.metrics = metrics
+        self.info = info
+
+    def to_brax_state(self) -> "ICLandBraxState":
+        """Converts an ICLandState to a Brax base State."""
+        model = self.pipeline_state.mjx_model
+        data = self.pipeline_state.mjx_data
+
+        # Adapted from m_pipeline.init (brax/mjx/pipeline.py)
+        # Its init function calls mjx.forward, which crashes
+        q, qd = data.qpos, data.qvel
+        x = Transform(pos=data.xpos[1:], rot=data.xquat[1:])
+        cvel = Motion(vel=data.cvel[1:, 3:], ang=data.cvel[1:, :3])
+        offset = data.xpos[1:, :] - data.subtree_com[model.body_rootid[1:]]
+        offset = Transform.create(pos=offset)
+        xd = offset.vmap().do(cvel)
+        reformated_data = _reformat_contact(model, data)
+
+        return ICLandBraxState(
+            pipeline_state=mjx_state(
+                q=q, qd=qd, x=x, xd=xd, **reformated_data.__dict__
+            ),
+            obs=self.observation,
+            reward=self.reward,
+            done=self.done,
+            metrics=self.metrics,
+            model=model,
+            component_ids=self.pipeline_state.component_ids,
+        )
+
+    def _tree_flatten(self) -> tuple[ICLandStateChildren, None]:
+        return (
+            self.pipeline_state,
+            self.observation,
+            self.reward,
+            self.done,
+            self.metrics,
+            self.info,
+        ), None
+
+    @classmethod
+    def _tree_unflatten(
+        cls: Type["ICLandState"], _: None, children: ICLandStateChildren
+    ) -> "ICLandState":
+        return cls(*children)
+
+
+jax.tree_util.register_pytree_node(
+    ICLandState, ICLandState._tree_flatten, ICLandState._tree_unflatten
+)
 
 
 class ICLandParams(PyTreeNode):  # type: ignore[misc]
@@ -85,3 +155,41 @@ class ICLandParams(PyTreeNode):  # type: ignore[misc]
         reward_function_signature = str(inspect.signature(self.reward_function))
 
         return f"ICLandParams(model={type(self.model).__name__}, game={self.game}, agent_count={self.agent_count}, reward_function={reward_function_name}{reward_function_signature})"
+
+
+class ICLandBraxState(State):  # type: ignore[misc]
+    """A stricter version of the brax base State.
+
+    pipeline_state isn't optional and is MJX specific
+    """
+
+    def __init__(
+        self,
+        pipeline_state: mjx_state,
+        obs: jax.Array,
+        reward: jax.Array,
+        done: jax.Array,
+        metrics: dict[str, jax.Array],
+        model: System,
+        component_ids: jax.Array,
+    ) -> None:
+        """Initializes the state with the given values."""
+        self.model = model
+        self.component_ids = component_ids
+        super().__init__(pipeline_state, obs, reward, done, metrics)
+
+    def to_icland_state(self) -> ICLandState:
+        """Converts a Brax State to an ICLandState."""
+        # In m_pipeline.step, mjx.step is called directly on the brax state
+        return ICLandState(
+            pipeline_state=PipelineState(
+                mjx_model=self.model,
+                mjx_data=self.pipeline_state,
+                component_ids=self.component_ids,
+            ),
+            observation=self.obs,
+            reward=self.reward,
+            done=self.done,
+            metrics=self.metrics,
+            info=self.info,
+        )
