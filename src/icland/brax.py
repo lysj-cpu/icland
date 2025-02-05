2aa173ffe0f1f3e232b4d9bf17204ecd1a97524e
harens <12570877+harens@users.noreply.github.com>
2025-02-05 15:02:40 +0000

    feat: add brax to icland adapter
    Convert brax states so that we can use our internal step functions

--- src/icland/brax.py
+++ src/icland/brax.py
@@ -1,9 +1,12 @@
 """ICLand Brax environment."""
 
+from typing import Optional
+
 import jax
-from brax import base
+from brax.base import Motion, System, Transform
 from brax.envs.base import Env, ObservationSize, State
-from brax.mjx import base as mjx_base
+from brax.io import mjcf
+from brax.mjx.base import State as mjx_state
 from brax.mjx.pipeline import _reformat_contact
 
 import icland
@@ -17,17 +20,15 @@ def icland_to_brax_adapter(icland_state: icland.ICLandState) -> State:
     # Adapted from m_pipeline.init (brax/mjx/pipeline.py)
     # Its init function calls mjx.forward, which crashes
     q, qd = data.qpos, data.qvel
-    x = base.Transform(pos=data.xpos[1:], rot=data.xquat[1:])
-    cvel = base.Motion(vel=data.cvel[1:, 3:], ang=data.cvel[1:, :3])
+    x = Transform(pos=data.xpos[1:], rot=data.xquat[1:])
+    cvel = Motion(vel=data.cvel[1:, 3:], ang=data.cvel[1:, :3])
     offset = data.xpos[1:, :] - data.subtree_com[model.body_rootid[1:]]
-    offset = base.Transform.create(pos=offset)
+    offset = Transform.create(pos=offset)
     xd = offset.vmap().do(cvel)
     reformated_data = _reformat_contact(model, data)
 
     return State(
-        pipeline_state=mjx_base.State(
-            q=q, qd=qd, x=x, xd=xd, **reformated_data.__dict__
-        ),
+        pipeline_state=mjx_state(q=q, qd=qd, x=x, xd=xd, **reformated_data.__dict__),
         obs=icland_state.observation,
         reward=icland_state.reward,
         done=icland_state.done,
@@ -35,44 +36,62 @@ def icland_to_brax_adapter(icland_state: icland.ICLandState) -> State:
     )
 
 
+def brax_to_icland_adapter(
+    brax_state: State, model: System, agent_count: int
+) -> icland.ICLandState:
+    """Converts a Brax base State to an ICLandState."""
+    # In m_pipeline.step, mjx.step is called directly on the brax state
+    return icland.ICLandState(
+        pipeline_state=icland.PipelineState(
+            mjx_model=model,
+            mjx_data=brax_state.pipeline_state,
+            # TODO: Consider caching agent calculation
+            component_ids=icland.collect_agent_components(model, agent_count),
+        ),
+        observation=brax_state.obs,
+        reward=brax_state.reward,
+        done=brax_state.done,
+        metrics=brax_state.metrics,
+        info=brax_state.info,
+    )
+
+
 class ICLand(Env):  # type: ignore[misc]
     """ICLand Brax environment."""
 
-    def __init__(self, rng: jax.Array) -> None:
+    def __init__(self, rng: jax.Array, params: Optional[icland.ICLandParams]) -> None:
         """Initializes the environment with a random seed."""
-        self.params = icland.sample(rng)
+        if params is None:
+            params = icland.sample(rng)
+        self.params = params
+        self._sys = mjcf.load_model(self.params.model)
 
     def reset(self, rng: jax.Array) -> State:
         """Resets the environment to an initial state."""
         initial_state = icland.init(rng, self.params)
-
-        # self._sys = mjcf.load_model(self.params.model)
-        # self._action_size = self._sys.act_size
-
         return icland_to_brax_adapter(initial_state)
 
     def step(self, state: State, action: jax.Array) -> State:
         """Run one timestep of the environment's dynamics."""
         key = jax.random.PRNGKey(0)
-        return icland_to_brax_adapter(icland.step(key, state, self.params, action))
+        return icland_to_brax_adapter(
+            icland.step(
+                key,
+                brax_to_icland_adapter(state, self._sys, self.params.agent_count),
+                self.params,
+                action,
+            )
+        )
 
     @property
     def observation_size(self) -> ObservationSize:
         """The size of the observation vector returned in step and reset."""
         return icland.AGENT_OBSERVATION_DIM
-        # From https://github.com/google/brax/blob/296184a1c77818b1988ebfe07539affe9a07db18/brax/envs/base.py#L145
-        # rng = jax.random.PRNGKey(0)
-        # reset_state = self.unwrapped.reset(rng)
-        # obs = reset_state.obs
-        # if isinstance(obs, jax.Array):
-        #     return obs.shape[-1]
-        # return jax.tree_util.tree_map(lambda x: x.shape, obs)
 
     @property
     def action_size(self) -> int:
         """The size of the action vector expected by step."""
         return icland.AGENT_ACTION_SPACE_DIM
-        # return self._sys.act_size()
 
     @property
     def backend(self) -> str:
--- tests/brax.py
+++ tests/brax.py
@@ -1,14 +1,14 @@
 """A temporary script showing how users would interact with the ICLand environment."""
 
 import jax
-from brax import envs
+from brax.envs import get_environment, register_environment
 
 from icland.brax import ICLand
 
 if __name__ == "__main__":
     # Adapted from https://colab.research.google.com/github/google-deepmind/mujoco/blob/main/mjx/tutorial.ipynb
-    envs.register_environment("icland", ICLand)
-    env = envs.get_environment("icland", rng=jax.random.key(42))
+    register_environment("icland", ICLand)
+    env = get_environment("icland", rng=jax.random.key(42))
     jit_reset = jax.jit(env.reset)
     jit_step = jax.jit(env.step)
 
